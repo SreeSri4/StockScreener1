4a20c76cf45bb3556758d9fcb58d446f361e31e1
SreeSri4 <101240173+SreeSri4@users.noreply.github.com>
2025-06-05 07:17:16 +0530

    Update chart-sidebar.tsx

--- client/src/components/chart-sidebar.tsx
+++ client/src/components/chart-sidebar.tsx
@@ -41,7 +41,7 @@ const TradingViewWidget = memo(({ symbol }: { symbol: string }) => {
               height: "100%",
               width: "100%",
               studies: [
-                "STD;SMA"
+                "STD;SMA (20, 50, 200)"
                  ]
             });
           } catch (err) {
