b6820b286f781e67741cb8f546c856c360d4ad19
SreeSri4 <101240173+SreeSri4@users.noreply.github.com>
2025-06-05 08:17:33 +0530

    Update chart-sidebar.tsx

--- client/src/components/chart-sidebar.tsx
+++ client/src/components/chart-sidebar.tsx
@@ -40,16 +40,12 @@ const TradingViewWidget = memo(({ symbol }: { symbol: string }) => {
               save_image: false,
               height: "100%",
               width: "100%",
-              // studies: [
-              //       "STD;SMA",
-              //       "STD;Relative%1Volume%1at%1Time",
-              //       "Volume@tv-basicstudies"
-              //    ]
+              studies: [
+                    "STD;SMA",
+                    "STD;Relative%1Volume%1at%1Time",
+                    "Volume@tv-basicstudies"
+                 ]
             });
-            tvWidget.onChartReady(() => {
-              const chart = tvWidget.chart(); // ✅ Get the chart object
-                chart.createStudy("Moving Average", false, false, [20]);
-              });
           } catch (err) {
             console.error("Error initializing TradingView widget:", err);
             setError(true);
